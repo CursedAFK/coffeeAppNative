82c837ea6a9ae2ebf3677c4fb018e864fdc43950
Abdulramon Lasisi <lasisiolalekan081@gmail.com>
2023-06-02 15:22:18 +0100

    project init

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import React from 'react'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Tab = createBottomTabNavigator()
+const Stack = createNativeStackNavigator()
+
+const Tabs = () => <Tab.Navigator></Tab.Navigator>
+
+const Stacks = () => (
+	<Stack.Navigator>
+		<Stack.Screen name='Home' component={HomeScreen} />
+		<Stack.Screen name='Product' component={ProductScreen} />
+	</Stack.Navigator>
+)
+
+const App = () => {
+	return (
+		<NavigationContainer>
+			<Stacks />
+		</NavigationContainer>
+	)
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
